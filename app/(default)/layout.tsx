abb437995f29399fe2a6a28af174e4fcc298ee72
jizero <jizero27@gmail.com>
2025-04-23 21:57:32 +0900

    feat: defaultLayout 생성 (#1)

---
+++ app/(default)/layout.tsx
@@ -0,0 +1,16 @@
+import Footer from "@/components/Footer/Footer";
+import Header from "@/components/Header/Header";
+
+export default function DefaultLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <div className="flex flex-col min-h-screen">
+      <Header />
+      <main className="w-full">{children}</main>
+      <Footer />
+    </div>
+  );
+}
