6c305fa5b0d25d90a1d9f97d7453622d2c98b509
jizero <jizero27@gmail.com>
2025-04-29 19:19:42 +0900

    feat: 상태 모달, 인터셉트 모달에 쓰일 컨테이너 분리 (#7)

--- app/(default)/layout.tsx
+++ app/(default)/layout.tsx
@@ -1,14 +1,17 @@
-import Header from "@/components/Header/Header";
+import Header from "@/app/(default)/_component/Header/Header";
 
 export default function DefaultLayout({
   children,
+  modal,
 }: {
   children: React.ReactNode;
+  modal: React.ReactNode;
 }) {
   return (
     <div className="flex flex-col min-h-screen">
       <Header />
       <main className="w-full">{children}</main>
+      {modal}
     </div>
   );
 }
