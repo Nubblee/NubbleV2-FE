f3460c2a75d8f3217aeafbc1d92b53a0990b593c
Sonseongoh <nobleser@naver.com>
2025-04-29 12:26:39 +0900

    fix: 변수명 변경 적용 (#14)

--- app/fonts.ts
+++ app/fonts.ts
@@ -2,7 +2,7 @@ import localFont from "next/font/local";
 
 export const pretendard = localFont({
   src: "../public/fonts/PretendardVariable.woff2",
-  variable: "--font-CSS변수명",
+  variable: "--font-pretendard",
   display: "swap",
   preload: true,
 });
