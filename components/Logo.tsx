4d16b5b5af85d4b2ff319169980407d832be5cd9
Sumin <tnalsdldkr2@naver.com>
2025-05-03 04:13:33 +0900

    feat: Logo 컴포넌트 구현 (#8)

---
+++ components/Logo.tsx
@@ -0,0 +1,25 @@
+import Image from 'next/image'
+import Link from 'next/link'
+
+type ColorType = 'white' | 'green'
+
+interface LogoProps {
+  color?: ColorType
+  width?: number
+  linkable?: boolean
+}
+
+const Logo = ({ color = 'green', width = 200, linkable = true }: LogoProps) => {
+  const src = color === 'green' ? '/nubble-green-logo.png' : '/nubble-white-logo.png'
+  const logo = <Image src={src} alt='Nubble Logo' width={width} height={width * 0.3} priority />
+
+  return linkable ? (
+    <Link href='/' className='inline-block cursor-pointer'>
+      {logo}
+    </Link>
+  ) : (
+    logo
+  )
+}
+
+export default Logo
