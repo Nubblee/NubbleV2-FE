be7d0798d5252143414b5ed680ed87a945207776
jizero <jizero27@gmail.com>
2025-05-03 18:46:03 +0900

    design: 에러페이지 홈으로 버튼 추가 (#37)

--- app/error/page.tsx
+++ app/error/page.tsx
@@ -1,6 +1,7 @@
 "use client";
 import { useRouter } from "next/navigation";
 import { cookierun } from "../fonts";
+import Button from "@/components/Button";
 
 const ErrorPage = () => {
   const router = useRouter();
@@ -18,16 +19,17 @@ const ErrorPage = () => {
       </div>
 
       <span className="font-bold text-lg">찾을 수 없는 페이지 입니다.</span>
-      <span className="font-normal text-gray-dark text-lg">
+      <span className="font-normal text-gray-dark text-lg mb-3">
         죄송합니다. 더 이상 존재하지 않는 페이지입니다.
       </span>
-      <button
+      <Button
+        variant="secondary"
         onClick={() => {
           router.push("/");
         }}
       >
         홈으로
-      </button>
+      </Button>
     </div>
   );
 };
