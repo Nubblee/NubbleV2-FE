103d4c1bb4b08a1948d6c6356fdc33997b06b729
Sonseongoh <nobleser@naver.com>
2025-05-03 21:21:27 +0900

    fix: 머지충돌 해결 (#17)

--- app/(default)/page.tsx
+++ app/(default)/page.tsx
@@ -1,12 +1,3 @@
-"use client";
-import Select from "@/components/Select";
-
-const options = [
-  { label: "옵션 1", value: "1" },
-  { label: "옵션 2", value: "2" },
-  { label: "옵션 3", value: "3" },
-];
-
 export default function Home() {
   return <div className="flex justify-center">너블 홈</div>;
 }
