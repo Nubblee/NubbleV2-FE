06bd9645db3f11e144b4f3dbc86ea2e84800d974
jizero <jizero27@gmail.com>
2025-05-04 23:06:35 +0900

    design: 로그인 페이지 퍼블리싱 (#34)

--- app/(auth)/login/page.tsx
+++ app/(auth)/login/page.tsx
@@ -1,5 +1,19 @@
+import Logo from "@/components/Logo";
+import LoginForm from "../_component/LoginForm";
+import AuthLink from "../_component/AuthLink";
+
 const page = () => {
-  return <div>로그인</div>;
+  return (
+    <div className="h-screen flex flex-col justify-center items-center">
+      <Logo width={200} />
+      <LoginForm />
+      <div className="flex gap-2 my-3 text-gray-light text-md font-light">
+        <AuthLink href="/signup">회원가입</AuthLink>
+        <span>|</span>
+        <AuthLink href={"/forgot-password"}>비밀번호 찾기</AuthLink>
+      </div>
+    </div>
+  );
 };
 
 export default page;
