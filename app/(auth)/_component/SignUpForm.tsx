28d39cfb5a3705481e9cecf749bca6ec8d814a8e
jizero <jizero27@gmail.com>
2025-05-07 01:06:10 +0900

    feat: 회원가입 페이지 퍼블리싱 (#33)

--- app/(auth)/_component/SignUpForm.tsx
+++ app/(auth)/_component/SignUpForm.tsx
@@ -1,17 +1,65 @@
+import Button from "@/components/Button";
 import Input from "@/components/Input/input";
-import Logo from "@/components/Logo";
+import Select from "@/components/Select";
+import TagButton from "@/components/TagGroup/TagButton";
 
 const SignUpForm = () => {
+  const emailOptions = [
+    { label: "직접입력", value: " " },
+    { label: "naver.com", value: "naver.com" },
+    { label: "gmail.com", value: "gmail.com" },
+    { label: "hanmail.com", value: "hanmail.com" },
+    { label: "kakao.com", value: "kakao.com" },
+  ];
+
   return (
     <div className="flex flex-col w-[350px] gap-5 items-center">
-      <Logo />
-      <span className="text-center">가입정보를 입력해주세요.</span>
-      <Input label="닉네임" variant="underline" />
-      <Input type="email" label="이메일 주소" variant="underline" />
-      <Input label="아이디" variant="underline" />
-      <Input label="비밀번호" variant="underline" />
-      <Input label="비밀번호 확인" variant="underline" />
-      <Input label="주소" variant="underline" />
+      <Input
+        label="닉네임"
+        className="border-gray-light"
+        variant="underline"
+        placeholder="닉네임을 입력하세요."
+      />
+      <div className="w-full h-[54px] relative">
+        <TagButton className="absolute w-[83px] bottom-1 right-0 cursor-pointer hover:bg-green-dark">
+          중복확인
+        </TagButton>
+        <Input
+          label="아이디"
+          className="absolute left-0 bottom-0 border-gray-light"
+          placeholder="아이디를 입력하세요."
+          variant="underline"
+        />
+      </div>
+      <Input
+        className="py-3 border-gray-light"
+        label="비밀번호"
+        placeholder="비밀번호를 입력하세요."
+      />
+      <Input
+        className="py-3 border-gray-light"
+        label="비밀번호 확인"
+        placeholder="비밀번호를 입력하세요."
+      />
+      <div className="flex items-center gap-1 w-full">
+        <Input
+          label="이메일"
+          type="email"
+          className="py-2 border-gray-light flex-grow"
+        />
+        <span className="mt-6">@</span>
+        <div className="w-full mt-6 flex gap-2">
+          {/* {selectItem === " " && (
+            <Input
+              className="py-2 border-gray-light flex-1"
+              placeholder="도메인을 입력하세요"
+            />
+          )} */}
+          <Select options={emailOptions} />
+        </div>
+      </div>
+      <Input label="주소" className="py-3 border-gray-light" />
+      <Button className="w-full py-3">회원가입</Button>
     </div>
   );
 };
--- app/(auth)/signup/page.tsx
+++ app/(auth)/signup/page.tsx
@@ -1,8 +1,10 @@
+import Logo from "@/components/Logo";
 import SignUpForm from "../_component/SignUpForm";
 
 const SignUp = () => {
   return (
-    <div className="h-screen flex flex-col justify-center items-center">
+    <div className="h-screen flex flex-col justify-center items-center gap-10">
+      <Logo width={200} />
       <SignUpForm />
     </div>
   );
