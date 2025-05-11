8408ee258ade743f85cb49cec02e51bc51d92504
jizero <jizero27@gmail.com>
2025-05-11 23:46:00 +0900

    fix: auth layout에 로고 추가 (#33)

--- app/(auth)/signup/page.tsx
+++ app/(auth)/signup/page.tsx
@@ -1,10 +1,8 @@
-import Logo from "@/components/Logo";
 import SignUpForm from "../_component/SignUpForm";
 
 const SignUp = () => {
   return (
-    <div className="h-screen flex flex-col justify-center items-center">
-      <Logo />
+    <div className="flex flex-col items-center">
       <span className="text-sm my-5 mb-10"> * 는 필수 입력값 입니다.</span>
       <SignUpForm />
     </div>
