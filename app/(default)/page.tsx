d0a93ccc8a9a04ec4446f9e35d5fe5ce99f18033
Sonseongoh <nobleser@naver.com>
2025-05-12 18:18:49 +0900

    feat:pull전 커밋(#45)

--- app/(default)/page.tsx
+++ app/(default)/page.tsx
@@ -1,5 +1,6 @@
 "use client";
 
+import StudyCard from "@/app/(default)/_component/StudyCard";
 import Button from "@/components/Button";
 import Input from "@/components/Input/input";
 
@@ -7,11 +8,11 @@ export default function HomePage() {
   return (
     <main className="flex flex-col items-center">
       {/*검색 필터 영역 */}
-      <section className="w-full bg-green-middle text-white py-10 px-4">
-        <div className="max-w-4/5 mx-auto space-y-6">
+      <section className="w-full bg-green-middle text-white py-4 px-4">
+        <div className="max-w-4/5 mx-auto space-y-4">
           {/* 제목 + 버튼 */}
           <div className="hidden md:flex flex-col md:flex-row items-center gap-4">
-            <h1 className="text-2xl md:text-3xl font-bold text-green-dark text-center md:text-left">
+            <h1 className="text-3xl font-bold text-green-dark">
               마음에 드는 스터디가 없으신가요?
             </h1>
             <Button
@@ -29,7 +30,7 @@ export default function HomePage() {
               <Input
                 key={placeholder}
                 placeholder={placeholder}
-                className="flex-1 text-black h-12"
+                className="flex-1 text-black h-9 md:h-12 pl-3"
                 variant="borderless"
               />
             ))}
@@ -47,7 +48,17 @@ export default function HomePage() {
       <section className="w-full max-w-6xl px-4 py-8">
         <h2 className="text-lg font-bold mb-4">내 스터디 🔒</h2>
         <div className="grid grid-cols-2 md:grid-cols-4 gap-4">
-          {/* <StudyCard /> * 2 */}
+          <StudyCard
+            type="recruiting"
+            icon="/nubble.png"
+            title="[JS] 알고리즘 뿌시기 스터디원 찾습니다!"
+            allMembers={5}
+            members={3}
+            level="Lv 1 - Lv 2"
+            days={["MON"]}
+            expireDay="2025.12.31"
+            onBookMark={() => {}}
+          />
         </div>
       </section>
 
@@ -55,7 +66,30 @@ export default function HomePage() {
       <section className="w-full max-w-6xl px-4 py-8">
         <h2 className="text-lg font-bold mb-4">모집중인 스터디 💬</h2>
         <div className="overflow-x-auto whitespace-nowrap">
-          <div className="inline-flex gap-4">{/* <StudyCard /> * N */}</div>
+          <div className="inline-flex gap-4">
+            <StudyCard
+              type="recruiting"
+              icon="/nubble.png"
+              title="[JS] 알고리즘 뿌시기 스터디원 찾습니다!"
+              allMembers={5}
+              members={3}
+              level="Lv 1 - Lv 2"
+              days={["MON"]}
+              expireDay="2025.12.31"
+              onBookMark={() => {}}
+            />
+            <StudyCard
+              type="recruiting"
+              icon="/nubble.png"
+              title="[JS] 알고리즘 뿌시기 스터디원 찾습니다!"
+              allMembers={5}
+              members={3}
+              level="Lv 1 - Lv 2"
+              days={["MON"]}
+              expireDay="2025.12.31"
+              onBookMark={() => {}}
+            />
+          </div>
         </div>
       </section>
 
@@ -63,7 +97,26 @@ export default function HomePage() {
       <section className="w-full max-w-6xl px-4 py-8">
         <h2 className="text-lg font-bold mb-4">진행중인 스터디 🔥</h2>
         <div className="grid grid-cols-2 md:grid-cols-3 gap-4">
-          {/* <StudyCard /> * 6 */}
+          <StudyCard
+            type="active"
+            icon="/nubble.png"
+            title="[JS] 알고리즘 뿌시기 스터디원 찾습니다!"
+            allMembers={5}
+            members={3}
+            level="Lv 1 - Lv 2"
+            days={["MON"]}
+            onBookMark={() => {}}
+          />
+          <StudyCard
+            type="active"
+            icon="/nubble.png"
+            title="[JS] 알고리즘 뿌시기 스터디원 찾습니다!"
+            allMembers={5}
+            members={3}
+            level="Lv 1 - Lv 2"
+            days={["MON"]}
+            onBookMark={() => {}}
+          />
         </div>
       </section>
     </main>
