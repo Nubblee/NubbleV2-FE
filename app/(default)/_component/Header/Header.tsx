13e9d895dc0b6f245e844129901af1005deea7c3
jizero <jizero27@gmail.com>
2025-05-12 22:06:21 +0900

    fix: 레이아웃 main max-w-screen-xl 설정 (#35)

--- app/(default)/_component/Header/Header.tsx
+++ app/(default)/_component/Header/Header.tsx
@@ -1,34 +1,36 @@
-'use client'
+"use client";
 
-import Link from 'next/link'
-import Logo from '@/components/Logo'
-import { navItems } from '@/config/nav'
-import { usePathname } from 'next/navigation'
+import Link from "next/link";
+import Logo from "@/components/Logo";
+import { navItems } from "@/config/nav";
+import { usePathname } from "next/navigation";
 
 const Header = () => {
-  const pathname = usePathname()
-  const isHome = pathname === '/'
+  const pathname = usePathname();
+  const isHome = pathname === "/";
 
-  const bgColor = isHome ? 'bg-green-middle' : ''
-  const logoColor = isHome ? 'white' : 'green'
-  const textColor = isHome ? 'text-white' : 'text-green-middle'
+  const bgColor = isHome ? "bg-green-middle" : "";
+  const logoColor = isHome ? "white" : "green";
+  const textColor = isHome ? "text-white" : "text-green-middle";
 
   return (
-    <header className={`flex items-center justify-between px-12 py-4 ${bgColor}`}>
-      <Logo color={logoColor} />
-      <nav className='flex gap-6'>
-        {navItems.map((item) => (
-          <Link
-            key={item.title}
-            href={item.href}
-            className={`font-semibold ${textColor} hover:font-extrabold`}
-          >
-            {item.title}
-          </Link>
-        ))}
-      </nav>
+    <header className={`flex px-12 py-4 ${bgColor}`}>
+      <div className="flex items-center justify-between max-w-screen-xl w-full mx-auto">
+        <Logo color={logoColor} />
+        <nav className="flex gap-6">
+          {navItems.map((item) => (
+            <Link
+              key={item.title}
+              href={item.href}
+              className={`font-semibold ${textColor} hover:font-extrabold`}
+            >
+              {item.title}
+            </Link>
+          ))}
+        </nav>
+      </div>
     </header>
-  )
-}
+  );
+};
 
-export default Header
+export default Header;
