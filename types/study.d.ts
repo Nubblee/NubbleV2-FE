4a9a67d5c7d894fefde397e0fbc3625d9b0e646d
jizero <jizero27@gmail.com>
2025-05-13 23:30:22 +0900

    feat: studyGroup interface 생성 (#35)

--- types/study.d.ts
+++ types/study.d.ts
@@ -1,4 +1,10 @@
-export type StudyCardType = "recruiting" | "active";
+import { DayType } from "./day";
+
+//스터디 상태
+export type StudyCardType = "RECRUITING" | "ACTIVE" | "ENDED";
+
+//스터디 진행방식
+export type MeetingType = "ONLINE" | "OFFLINE" | "HYBRID";
 
 export interface StudyCardProps {
   id: string;
@@ -11,4 +17,21 @@ export interface StudyCardProps {
   days: string[];
   expireDay?: string;
   className?: string;
+}
+
+export interface StudyGroup {
+  studyGroupId: string;
+  name: string;
+  description: string;
+  capacity: number;
+  startDate: string;
+  endDate: string | null;
+  status: StudyCardType;
+  languages: string[];
+  mainLanguage: string;
+  difficultyLevels: string[];
+  problemPlatforms: string[];
+  meetingType: MeetingType;
+  meetingRegion: string | null;
+  mainMeetingDays: DayType[];
 }
