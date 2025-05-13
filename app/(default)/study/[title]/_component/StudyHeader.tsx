f5b9c8d14b6831624ad2aeae792e526b09a0afcb
jizero <jizero27@gmail.com>
2025-05-13 19:08:07 +0900

    feat: studyHeader 컴포넌트 구현 (#36)

---
+++ app/(default)/study/[title]/_component/StudyHeader.tsx
@@ -0,0 +1,63 @@
+import ShowDay from "@/app/(default)/_component/ShowDay";
+import ProfileImage from "@/components/ProfileImage";
+import TagGroup from "@/components/TagGroup/TagGroup";
+import Image from "next/image";
+
+interface StudyInfoProps {
+  icon: string;
+  language: string;
+  levels: string[];
+  title: string;
+  days: string[];
+  members?: membersProps[];
+}
+
+interface membersProps {
+  name: string;
+  image: string;
+}
+
+const StudyInfo = ({
+  icon,
+  language,
+  levels,
+  title,
+  days,
+  members,
+}: StudyInfoProps) => {
+  return (
+    <div className="w-full flex gap-13">
+      <Image
+        className="rounded-lg"
+        src={icon}
+        alt={language}
+        width={220}
+        height={220}
+      />
+      <div className="flex flex-col gap-4 w-full">
+        <TagGroup options={levels} readonly />
+        <span className="font-bold text-3xl w-[45%] line-clamp-2 break-words">
+          {title}
+        </span>
+        <div className="flex gap-3 items-center">
+          <span className="font-base text-base">진행요일</span>
+          <ShowDay days={days} />
+        </div>
+        {members && (
+          <div className="flex gap-3 items-center">
+            <span className="font-base text-base">스터디원</span>
+            {members.map((member) => (
+              <ProfileImage
+                key={member.image}
+                src={member.image}
+                alt={member.name}
+              />
+            ))}
+          </div>
+        )}
+      </div>
+    </div>
+  );
+};
+
+export default StudyInfo;
