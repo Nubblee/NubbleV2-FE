9341dc3a6dad0ef1ebc0aa0025ba1bdd44fbc2de
jizero <jizero27@gmail.com>
2025-05-15 03:17:33 +0900

    feat: 스터디원 관리, 승인요청 관리 탭 section 구현 (#36)

---
+++ app/(default)/study/[title]/management/_component/ClickTabSection.tsx
@@ -0,0 +1,96 @@
+"use client";
+
+import Tab from "@/app/(default)/_component/Tab";
+import { TabItems } from "@/types/tab";
+import MemberCard from "../../_component/MemberCard";
+import React from "react";
+import { useParams } from "next/navigation";
+import ApproveList from "./ApproveList";
+
+const generatedTabs = (title: string): TabItems[] => [
+  {
+    name: "스터디원 관리",
+    link: `/study/${title}/management/members`,
+  },
+  {
+    name: "승인요청 관리",
+    link: `/study/${title}/management/approve`,
+  },
+];
+
+const MemberData = [
+  {
+    name: "명수",
+    written: 32,
+    reviews: 18,
+    recently: "2025.01.19",
+  },
+  {
+    name: "명수",
+    written: 32,
+    reviews: 18,
+    recently: "2025.01.19",
+  },
+  {
+    name: "명수",
+    written: 32,
+    reviews: 18,
+    recently: "2025.01.19",
+  },
+  {
+    name: "명수",
+    written: 32,
+    reviews: 18,
+    recently: "2025.01.19",
+  },
+  {
+    name: "명수",
+    written: 32,
+    reviews: 18,
+    recently: "2025.01.19",
+  },
+  {
+    name: "명수",
+    written: 32,
+    reviews: 18,
+    recently: "2025.01.19",
+  },
+  {
+    name: "명수",
+    written: 32,
+    reviews: 18,
+    recently: "2025.01.19",
+  },
+  {
+    name: "명수",
+    written: 32,
+    reviews: 18,
+    recently: "2025.01.19",
+  },
+  {
+    name: "명수",
+    written: 32,
+    reviews: 18,
+    recently: "2025.01.19",
+  },
+];
+
+const ClickTabSection = () => {
+  const params = useParams();
+  const studyTitle = params.title as string;
+
+  return (
+    <div className="flex flex-col gap-4">
+      <Tab tabItems={generatedTabs(studyTitle)} />
+      <div className="px-10 w-full grid grid-cols-[repeat(auto-fill,_minmax(200px,_1fr))] gap-6">
+        {params.tab === "members" ? (
+          MemberData.map((member, idx) => <MemberCard key={idx} {...member} />)
+        ) : (
+          <ApproveList />
+        )}
+      </div>
+    </div>
+  );
+};
+
+export default ClickTabSection;
