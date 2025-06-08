8e96f48c5798d64c76d9330e6699d51fd4d9c2bd
jizerozz <jizero27@gmail.com>
2025-06-08 17:40:45 +0900

    refactor: 회원가입 메일 주소, 주소 변수 signup config로 이동 (#59)

--- app/(auth)/_component/SignUpForm.tsx
+++ app/(auth)/_component/SignUpForm.tsx
@@ -3,37 +3,11 @@ import Button from "@/components/Button";
 import Input from "@/components/Input/input";
 import Select from "@/components/Select";
 import TagButton from "@/components/TagGroup/TagButton";
+import { addressOptions, emailOptions } from "@/config/signup";
 import { useRouter } from "next/navigation";
 
 const SignUpForm = () => {
   const router = useRouter();
-  const emailOptions = [
-    { label: "직접입력", value: " " },
-    { label: "@naver.com", value: "@naver.com" },
-    { label: "@gmail.com", value: "@gmail.com" },
-    { label: "@hanmail.com", value: "@hanmail.com" },
-    { label: "@kakao.com", value: "@kakao.com" },
-  ];
-
-  const addressOptions = [
-    { label: "서울특별시", value: "seoul" },
-    { label: "부산광역시", value: "busan" },
-    { label: "대구광역시", value: "daegu" },
-    { label: "인천광역시", value: "incheon" },
-    { label: "광주광역시", value: "gwangju" },
-    { label: "대전광역시", value: "daejeon" },
-    { label: "울산광역시", value: "ulsan" },
-    { label: "세종특별자치시", value: "sejong" },
-    { label: "경기도", value: "gyeonggi" },
-    { label: "강원도", value: "gangwon" },
-    { label: "충청북도", value: "chungbuk" },
-    { label: "충청남도", value: "chungnam" },
-    { label: "전라북도", value: "jeonbuk" },
-    { label: "전라남도", value: "jeonnam" },
-    { label: "경상북도", value: "gyeongbuk" },
-    { label: "경상남도", value: "gyeongnam" },
-    { label: "제주특별자치도", value: "jeju" },
-  ];
 
   const handleSubmit = () => {
     router.push("/signup/interests");
@@ -91,7 +65,7 @@ const SignUpForm = () => {
         </label>
         <Select options={addressOptions} />
       </div>
-      <Button className="w-full py-3" onClick={handleSubmit}>
+      <Button className="w-full py-3t" onClick={handleSubmit}>
         가입하기
       </Button>
     </div>
