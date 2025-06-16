da477a306fc1f0e6ccf1164fb7de0a6ecc94b320
Sonseongoh <nobleser@naver.com>
2025-06-17 02:16:57 +0900

    fix: 윈도우에서만 보이는 /b 문자 제거(#64)

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,7 +2,7 @@ import type { Metadata } from "next";
 import "./globals.css";
 import { pretendard } from "./fonts";
 import ToastContainer from "@/components/ToastContainer";
-import AuthProvier from "@/components/\bauth/auth-provier";
+import AuthProvier from "@/components/auth/auth-provier";
 
 export const metadata: Metadata = {
   title: "Nubble",
