2236c366f4a8a1163a031314bbcb457d7d75dad3
jizero <jizero27@gmail.com>
2025-06-19 01:10:29 +0900

    feat: 로그인시 헤더에 프로필 렌더링 처리, 드롭다운 컴포넌트 구현 및 적용 (#60)

---
+++ app/(default)/_component/Header/dropdown.tsx
@@ -0,0 +1,53 @@
+import Link from "next/link";
+import { useEffect, useRef } from "react";
+
+interface DropDownListProps {
+  label: string;
+  link?: string;
+  onClick?: () => void;
+}
+
+interface DropDownProps {
+  lists: DropDownListProps[];
+  onClose?: () => void;
+}
+
+export const DropDown = ({ lists, onClose }: DropDownProps) => {
+  const ref = useRef<HTMLDivElement>(null);
+
+  useEffect(() => {
+    const handleOverlay = (e: MouseEvent) =>
+      ref.current && !ref.current.contains(e.target as Node) && onClose?.();
+
+    window.addEventListener("mousedown", handleOverlay);
+    return () => window.removeEventListener("mousedown", handleOverlay);
+  }, [onClose]);
+
+  return (
+    <div
+      ref={ref}
+      className="absolute right-2 mt-10 w-28 bg-white border border-gray-200 rounded shadow z-10"
+    >
+      <ul className="text-sm text-gray-700">
+        {lists.map((list, idx) => (
+          <li key={idx} className="hover:bg-gray-100 px-4 py-2 cursor-pointer">
+            {list.link ? (
+              <Link href={list.link}>{list.label}</Link>
+            ) : list.onClick ? (
+              <button
+                onClick={() => {
+                  list.onClick?.();
+                  onClose?.();
+                }}
+              >
+                {list.label}
+              </button>
+            ) : (
+              <span>{list.label}</span>
+            )}
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+};
